5d03ccd53e508331994ee41a6b134601a91b27b8
Trishan Fernando <sanjanatrishan@gmail.com>
2024-05-15 23:32:32 +0530

    Updated the code

--- pwgen.py
+++ pwgen.py
@@ -1,4 +1,38 @@
 import random
+import string
 
-print("Hello, World !")
-print("This is a password Genrator")
+print("This is a Small Password Genrator")
+
+#Ask for user inputs
+count = int(input("How many letters would you like in your password ? "))
+count1 = int(input("How many Symbols would you like in your password ? "))
+count2 = int(input("How many Digits would you like in your password ? "))
+
+#Declare the variables
+letters = string.ascii_letters
+symbols = string.punctuation
+digits = string.digits
+password = []
+
+
+
+while count <= 0 :
+    count = int(input("How many letters would you like in your password ? "))
+while count1 <= 0 :
+    count1 = int(input("How many Symbols would you like in your password ? "))
+while count2 <= 0 :
+    count2 = int(input("How many Digits would you like in your password ? "))
+     
+
+for _ in range(count):              #Generate random letters
+    password.append(random.choice(letters))
+    
+for _ in range(count1):             #Generate random Numbers
+    password.append(random.choice(digits))
+    
+for _ in range(count2):             #Generate random Symbols
+    password.append(random.choice(symbols))
+
+random.shuffle(password)            #Shuffle the password
+password1 = [print(i,end="") for i in password]   #print the password using list comprehension
+print("".join(password),"\nthis is the 1nd password \n")     #convert the list into a string & print it
